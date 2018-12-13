4d7b9cf6f0a6d366898b48e52add746cf83780c3
futcms <34991938+futcms@users.noreply.github.com>
2018-12-13 20:20:40 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+*\
+Ganti Dengan data kalian Masing-masing 
+*\
+$config = [
+"imei" => "862120086425955",
+"sign" => "f05e0826ab15df66f0iyrx74yinjf7a59225870d7",
+"token" => "a2cfdf20f2efuhdstuoff4d98970c663",
+"uuid" => "692bjhrd3-b0fd-4d3c-b7b6-3712hgdtir43"
+];
